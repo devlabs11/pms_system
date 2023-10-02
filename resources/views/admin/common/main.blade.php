e28e5dcd93daec1fe2ecf8151c39c52e586474d5
unknown <nishant.karale@neosoftmail.com>
2023-10-02 13:01:50 +0530

    frontend-done

---
+++ resources/views/admin/common/main.blade.php
@@ -0,0 +1,9 @@
+
+
+@include('admin.common.header')
+@include('admin.common.sidebar')
+<body>
+    @yield('containes')
+</body>
+@include('admin.common.footer')
+
---
+++ resources/views/admin/common/sidebar.blade.php
@@ -0,0 +1,157 @@
+
+<div class="app-wrapper flex-column flex-row-fluid" id="kt_app_wrapper">
+					<div id="kt_app_sidebar" class="app-sidebar flex-column" data-kt-drawer="true" data-kt-drawer-name="app-sidebar" data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="225px" data-kt-drawer-direction="start" data-kt-drawer-toggle="#kt_app_sidebar_mobile_toggle">
+						<div class="app-sidebar-logo px-6" id="kt_app_sidebar_logo">
+							<a href="../../demo1/dist/index.html">
+								<img alt="Logo" src="assets/media/logos/default-dark.svg" class="h-25px app-sidebar-logo-default" />
+								<img alt="Logo" src="assets/media/logos/default-small.svg" class="h-20px app-sidebar-logo-minimize" />
+							</a>
+						
+							<div id="kt_app_sidebar_toggle" class="app-sidebar-toggle btn btn-icon btn-shadow btn-sm btn-color-muted btn-active-color-primary body-bg h-30px w-30px position-absolute top-50 start-100 translate-middle rotate" data-kt-toggle="true" data-kt-toggle-state="active" data-kt-toggle-target="body" data-kt-toggle-name="app-sidebar-minimize">
+								<i class="ki-duotone ki-double-left fs-2 rotate-180">
+									<span class="path1"></span>
+									<span class="path2"></span>
+								</i>
+							</div>
+							
+						</div>
+						
+						<div class="app-sidebar-menu overflow-hidden flex-column-fluid">
+							
+							<div id="kt_app_sidebar_menu_wrapper" class="app-sidebar-wrapper hover-scroll-overlay-y my-5" data-kt-scroll="true" data-kt-scroll-activate="true" data-kt-scroll-height="auto" data-kt-scroll-dependencies="#kt_app_sidebar_logo, #kt_app_sidebar_footer" data-kt-scroll-wrappers="#kt_app_sidebar_menu" data-kt-scroll-offset="5px" data-kt-scroll-save-state="true">
+								<div class="menu menu-column menu-rounded menu-sub-indention px-3" id="#kt_app_sidebar_menu" data-kt-menu="true" data-kt-menu-expand="false">
+									<div data-kt-menu-trigger="click" class="menu-item here show menu-accordion">
+										<span class="menu-link">
+											<span class="menu-icon">
+												<i class="ki-duotone ki-element-11 fs-2">
+													<span class="path1"></span>
+													<span class="path2"></span>
+													<span class="path3"></span>
+													<span class="path4"></span>
+												</i>
+											</span>
+											<span class="menu-title">ACL</span>
+											<span class="menu-arrow"></span>
+										</span>
+										<div class="menu-sub menu-sub-accordion">
+											<div class="menu-item">
+												<a class="menu-link active" href="../../demo1/dist/index.html">
+													<span class="menu-bullet">
+														<span class="bullet bullet-dot"></span>
+													</span>
+													<span class="menu-title">ADMINS</span>
+
+												</a>
+											</div>
+											<div class="menu-item">
+												<a class="menu-link" href="../../demo1/dist/dashboards/ecommerce.html">
+													<span class="menu-bullet">
+														<span class="bullet bullet-dot"></span>
+													</span>
+													<span class="menu-title">USERS</span>
+												</a>
+											</div>
+											<div class="menu-item">
+												<a class="menu-link" href="../../demo1/dist/dashboards/projects.html">
+													<span class="menu-bullet">
+														<span class="bullet bullet-dot"></span>
+													</span>
+													<span class="menu-title">PUBLISHED</span>
+												</a>
+											</div>
+											<div class="menu-item">
+												<a class="menu-link" href="../../demo1/dist/dashboards/online-courses.html">
+													<span class="menu-bullet">
+														<span class="bullet bullet-dot"></span>
+													</span>
+													<span class="menu-title">NOT PUBLISHED</span>
+												</a>
+											</div>
+											<div class="menu-item">
+												
+											</div>
+											
+											
+										</div>
+									</div>
+									<div class="menu-item pt-5">
+										<div class="menu-content">
+											<span class="menu-heading fw-bold text-uppercase fs-7">USER SECTION</span>
+										</div>
+									</div>
+									<div data-kt-menu-trigger="click" class="menu-item menu-accordion">
+										<span class="menu-link">
+											<span class="menu-icon">
+												<i class="ki-duotone ki-address-book fs-2">
+													<span class="path1"></span>
+													<span class="path2"></span>
+													<span class="path3"></span>
+												</i>
+											</span>
+											<span class="menu-title">USERS</span>
+											<span class="menu-arrow"></span>
+										</span>
+										<div class="menu-sub menu-sub-accordion">
+											<div class="menu-item">
+												<a class="menu-link" href="../../demo1/dist/pages/user-profile/overview.html">
+													<span class="menu-bullet">
+														<span class="bullet bullet-dot"></span>
+													</span>
+													<span class="menu-title">View</span>
+												</a>
+											</div>
+											<div class="menu-item">
+												<a class="menu-link" href="../../demo1/dist/pages/user-profile/projects.html">
+													<span class="menu-bullet">
+														<span class="bullet bullet-dot"></span>
+													</span>
+													<span class="menu-title">Modifications</span>
+												</a>
+											</div>
+											<div class="menu-item">
+												<a class="menu-link" href="../../demo1/dist/pages/user-profile/campaigns.html">
+													<span class="menu-bullet">
+														<span class="bullet bullet-dot"></span>
+													</span>
+													<span class="menu-title">Published Users</span>
+												</a>
+											</div>
+											<div class="menu-item">
+												<a class="menu-link" href="../../demo1/dist/pages/user-profile/campaigns.html">
+													<span class="menu-bullet">
+														<span class="bullet bullet-dot"></span>
+													</span>
+													<span class="menu-title">Not Published Users</span>
+												</a>
+											</div>
+											
+										</div>
+									</div>
+									
+									
+									
+									
+									<div class="menu-item pt-5">
+										<div class="menu-content">
+											<span class="menu-heading fw-bold text-uppercase fs-7">Help</span>
+										</div>
+									</div>
+									<div class="menu-item">
+										<a class="menu-link" href="https://preview.keenthemes.com/html/metronic/docs/base/utilities" target="_blank">
+											<span class="menu-icon">
+												<i class="ki-duotone ki-rocket fs-2">
+													<span class="path1"></span>
+													<span class="path2"></span>
+												</i>
+											</span>
+											<span class="menu-title">CONTACT US</span>
+										</a>
+									</div>
+									
+									
+								</div>
+							</div>
+							
+						</div>
+
+					</div>
