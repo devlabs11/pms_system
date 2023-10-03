530ecc036e5b50d8f38a4f025183cd212bd3bd48
unknown <nishant.karale@neosoftmail.com>
2023-10-03 13:16:40 +0530

    auth-done

--- resources/views/admin/common/header.blade.php
+++ resources/views/admin/common/header.blade.php
@@ -4511,7 +4511,7 @@
 								<div class="app-navbar-item ms-1 ms-md-3" id="kt_header_user_menu_toggle">
 									<!--begin::Menu wrapper-->
 									<div class="cursor-pointer symbol symbol-30px symbol-md-40px" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
-										<img src="assets/media/avatars/blank.png" alt="user" />
+									<span class="badge badge-success" style="margin-right:10px;">Active</span><img src="assets/media/avatars/blank.png" alt="user" />
 									</div>
 									<!--begin::User account menu-->
 									<div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-color fw-semibold py-4 fs-6 w-275px" data-kt-menu="true">
